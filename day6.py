ba7b182444d21276d691a71492e21b73af11dbc0
Leander <leander.sabel@gmail.com>
2024-12-07 22:30:46 +0000

    Solution for day 6

---
+++ day6.py
@@ -0,0 +1,89 @@
+# https://adventofcode.com/2024/day/6
+from collections import deque
+from copy import deepcopy
+from enum import Enum
+
+from solution import Solution
+
+direction = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
+turn_right = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
+
+class Status(Enum):
+    MOVING = 0
+    OUT = 1
+    LOOP = 2
+
+class Day6Solution(Solution):
+
+    def parse_input(self):
+        return [list(row) for row in self.input_data.split('\n')]
+
+    def solve_part1(self):
+        grid = deepcopy(self.parsed_input)
+        guard = next((r, row.index('^')) for r, row in enumerate(grid) if '^' in row)
+
+        while guard not in [Status.OUT, Status.LOOP]:
+            guard = move_guard(grid, guard, [])
+
+        return sum(row.count('x') for row in grid) + 1
+
+
+    def solve_part2(self):
+        grid = self.parsed_input
+        variants = generate_variants(grid)
+        loops = 0
+
+        while len(variants) > 0:
+            visited = []
+            guard = next((r, row.index('^')) for r, row in enumerate(grid) if '^' in row)
+            var_grid = variants.pop()
+
+            while guard not in [Status.OUT, Status.LOOP]:
+                guard = move_guard(var_grid, guard, visited)
+
+            if guard == Status.LOOP:
+                loops += 1
+
+        return loops
+
+def move_guard(grid, guard, visited):
+    r, c = guard
+
+    while collision(grid, r, c):
+        grid[r][c] = turn_right.get(grid[r][c])
+
+    dr, dc = direction.get(grid[r][c])
+
+    if 0 <= r + dr < len(grid) and 0 <= c + dc < len(grid[0]):
+        if (r, c, grid[r][c]) not in visited:
+            visited.append((r, c, grid[r][c]))
+            grid[r + dr][c + dc] = grid[r][c]
+            grid[r][c] = 'x'                        # For part 1
+            return r + dr, c + dc
+        else: return Status.LOOP                    # For part 2
+    else: return Status.OUT
+
+def collision(grid, r, c):
+    dr, dc = direction.get(grid[r][c])
+
+    if 0 <= r + dr < len(grid) and 0 <= c + dc < len(grid[0]):
+        return grid[r + dr][c + dc] == '#'
+
+def generate_variants(grid):
+    variants = []
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == '.':
+                new_grid = [list(row) for row in grid]
+                new_grid[r][c] = '#'
+                variants.append(new_grid)
+
+    return variants
+
+
+if __name__ == "__main__":
+    solution = Day6Solution(day=6, example=1)
+    solution.run(part=1)
+    solution.run(part=2)
+
