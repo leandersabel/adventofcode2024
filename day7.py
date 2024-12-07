7f946f0bb2bf6164f7d7782b3bf4ad1b75ac581b
Leander <leander.sabel@gmail.com>
2024-12-07 23:12:39 +0000

    Performance enhancements for day 7

--- day7.py
+++ day7.py
@@ -1,4 +1,7 @@
 # https://adventofcode.com/2024/day/7
+from functools import lru_cache
+from multiprocessing import Pool
+
 from itertools import product
 
 import utils
@@ -21,7 +24,10 @@ class Day7Solution(Solution):
 
             combinations = generate_all_combinations(numbers, available_operators_part_1)
 
-            if any(evaluate_left_to_right(combination) == test_value for combination in combinations):
+            with Pool() as pool:
+                results = pool.map(evaluate_left_to_right, combinations)
+
+            if test_value in results:
                 valid_test_values += test_value
 
         return valid_test_values
@@ -35,7 +41,11 @@ class Day7Solution(Solution):
 
             combinations = generate_all_combinations(numbers, available_operators_part_2)
 
-            if any(evaluate_left_to_right(combination) == test_value for combination in combinations):
+            with Pool() as pool:
+                results = pool.starmap(
+                    evaluate_combination, [(combination, test_value) for combination in combinations])
+
+            if any(results):
                 valid_test_values += test_value
 
         return valid_test_values
@@ -43,17 +53,30 @@ class Day7Solution(Solution):
 def generate_all_combinations(n, o):
     return ["".join(f"{num} {op} " for num, op in zip(n, ops)) + str(n[-1]) for ops in product(o, repeat=len(n) - 1)]
 
+def evaluate_combination(combination, test_value):
+    return evaluate_left_to_right(combination) == test_value
+
+@lru_cache(maxsize=None)
 def evaluate_left_to_right(expression):
     tokens = expression.split()
-    result = int(tokens[0])
+    result = int(tokens[0])  # Start with the first number
 
     for i in range(1, len(tokens), 2):
-        operator = '' if tokens[i] == '||' else f" {tokens[i]} "  # For part II
-        result = eval(f"{result}{operator}{tokens[i + 1]}")
+        operator = tokens[i]
+        operand = int(tokens[i + 1])
+
+        if operator == '+':
+            result += operand
+        elif operator == '*':
+            result *= operand
+        elif operator == '||':
+            result = int(str(result) + str(operand))
+
     return result
 
 
+
 if __name__ == "__main__":
-    solution = Day7Solution(day=7, example=1)
+    solution = Day7Solution(day=7, example=None)
     solution.run(part=1)
     solution.run(part=2)
